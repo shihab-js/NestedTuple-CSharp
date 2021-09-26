f66353d38a2905f6b6380daf6dbca96ae860955a
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-26 23:04:36 +0600

    Elements access successfully...

--- NestedTuple/Program.cs
+++ NestedTuple/Program.cs
@@ -8,6 +8,15 @@ namespace NestedTuple
         {
             //create nested tuple
             var numbers = Tuple.Create(1, 2, 3, Tuple.Create(4, 5, 6, 7), 8, 9, 10, 11);
+
+            //access the elements
+            Console.WriteLine(numbers.Item1); // returns 1
+            Console.WriteLine(numbers.Item2); // returns 2
+            Console.WriteLine(numbers.Item3); // returns 3
+            Console.WriteLine(numbers.Item4); // returns (4,5,6,7)
+            Console.WriteLine(numbers.Item4.Item1); // returns 4
+            Console.WriteLine(numbers.Item5); // returns 8
+            Console.WriteLine(numbers.Rest.Item1); //returns 11
         }
     }
 }
