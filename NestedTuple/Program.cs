c2a829ae159e6c399d2b1178abd7c73a69c34fbe
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-26 23:01:48 +0600

    Nested tuple has been created...

--- NestedTuple/Program.cs
+++ NestedTuple/Program.cs
@@ -6,7 +6,8 @@ namespace NestedTuple
     {
         static void Main(string[] args)
         {
-           
+            //create nested tuple
+            var numbers = Tuple.Create(1, 2, 3, Tuple.Create(4, 5, 6, 7), 8, 9, 10, 11);
         }
     }
 }
